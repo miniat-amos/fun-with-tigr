476b2f597b9b844d2a99da2183c83864d0dbc649
William "Amos" Confer <conferw@sunypoly.edu>
2025-10-03 23:21:42 -0400

    Add fun program that detects the ESC key

---
+++ 02_hello_esc.c
@@ -0,0 +1,28 @@
+/**
+ * Program: 02_hello_esc
+ * 
+ * This program shows how to detect the ESC key and one way a
+ * program could terminate when that happens.
+ */
+#include "tigr.h"
+
+const char title[] = "02_hello_esc";
+const char str[] = "Press the ESC key to exit...";
+
+int main(int argc, char *argv[]) {
+    Tigr *screen = tigrWindow(300, 200, title, TIGR_FIXED);
+
+    /**
+     * Terminate this main loop if the 'X' is clicked or ESC pressed
+     */
+    while (!tigrClosed(screen) && !tigrKeyDown(screen, TK_ESCAPE)) {
+        tigrClear(screen, tigrRGB(0xDD, 0xBB, 0xFF));
+        
+        tigrPrint(screen, tfont, 50, 75, tigrRGB(0xFF, 0xFF, 0xFF), str);        
+        tigrUpdate(screen);
+    }
+
+    tigrFree(screen);
+
+    return 0;
+}
