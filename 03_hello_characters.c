f0a3557cc3d040fac76fd31b7e1556333e3faa4a
William "Amos" Confer <conferw@sunypoly.edu>
2025-10-04 23:12:06 -0400

    Add fun program that reads and prints characters from the keyboard

---
+++ 03_hello_characters.c
@@ -0,0 +1,44 @@
+/**
+ * Program: 03_hello_characters
+ * 
+ * This program uses tigrReadChar() to detect printable character keypresses
+ * and prints the most recent to the screen.  This function does not detect
+ * "special key" presses (eg, Enter, arrow keys, fuction keys).  Instead,
+ * it detects aplha numeric keys, symbols, and just a couple non-printable
+ * characters, such as backspace and delete.
+ */
+#include "tigr.h"
+
+int main(int argc, char *argv[]) {
+    Tigr *screen = tigrWindow(300, 200, __FILE__, TIGR_FIXED);
+
+    const char str[] = "Press 'regular' keyboard keys:";
+    /**
+     * This string will be where we store the most recent character pressed.
+     * Every frame, we'll show this on the screen, so we initialize it to
+     * space (something we can't see) so before any key has been pressed,
+     * the program will appear to not be printing anything about a key.
+     */
+    char key_str[] = " ";
+
+    while (!tigrClosed(screen) && !tigrKeyDown(screen, TK_ESCAPE)) {
+        char key = (char)tigrReadChar(screen);
+        /**
+         * tigrReadChar() returns 0 if no key has been pressed, so wait
+         * until its return indicates a key has been presseed.
+         */
+        if(key != 0) {
+            /* A new key was detected.  Make it the one we show now. */
+            key_str[0] = key;
+        }
+
+        tigrClear(screen, tigrRGB(0xDD, 0xBB, 0xFF));
+        tigrPrint(screen, tfont, 50, 75, tigrRGB(0xFF, 0xFF, 0xFF), str);
+        tigrPrint(screen, tfont, 65, 90, tigrRGB(0xFF, 0xFF, 0xFF), key_str);
+        tigrUpdate(screen);
+    }
+
+    tigrFree(screen);
+
+    return 0;
+}
